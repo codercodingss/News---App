f8c5abb0e81219a0fc14683c3b23c4fc4308d162
Daniella <elisiella13@gmail.com>
2024-07-18 15:53:09 +0100

    code..

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:newssec/pages/home.dart';
+import 'package:newssec/utills/colors.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          brightness: Brightness.dark, primaryColor: AppColors.primary),
+      home: Home(),
+    );
+  }
+}
